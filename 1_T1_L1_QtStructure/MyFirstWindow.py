f2bc5bd8b69773283bdb5b6c0bd6afa263656238
vladuxa_23 <vlad_1995.02@mail.ru>
2021-02-27 16:25:35 +0300

    initial commit and add 1_T1_L1

---
+++ 1_T1_L1_QtStructure/MyFirstWindow.py
@@ -0,0 +1,17 @@
+import sys
+from PySide2 import QtWidgets
+
+
+class MyFirstWindow(QtWidgets.QMainWindow):
+
+    def __int__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    myWindow = MyFirstWindow()
+    myWindow.show()
+
+    app.exec_()
